c5f6cb29291827e03310b6495a167693f768ea3d
manjunathc9942-cell <manjunathc9942@gmail.com>
2026-07-13 23:04:03 +0530

    selenium_2

--- selenium_basics/browser_setup.py
+++ selenium_basics/browser_setup.py
@@ -8,6 +8,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.google.com")
+input_element = driver.find_element(By.ID, "input")
+wait = input_element.send_keys("demo automation testing").click()
+class_name = driver.find_element(By.CLASS_NAME, "LC20lb MBeuO DKV0Md").click()
