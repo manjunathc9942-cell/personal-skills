98fe1d31815e7137fa08c563b18519dda540ca9f
manjunathc9942-cell <manjunathc9942@gmail.com>
2026-07-14 00:04:23 +0530

    SELENIUM2

--- selenium_basics/browser_setup.py
+++ selenium_basics/browser_setup.py
@@ -2,6 +2,31 @@
 # toinstall selenium and webdriver-manager, run the following commands in your terminal:
 # pip3 install selenium
 # pip3 install webdriver-manager
+
+locators:
+# id: driver.find_element(By.ID, "value")
+# name: driver.find_element(By.NAME, "value")
+# CLASS_NAME: driver.find_element(By.CLASS_NAME, "value")
+# TAG_NAME: driver.find_element(By.TAG_NAME, "value")
+# LINK_TEXT: driver.find_element(By.LINK_TEXT, "value")
+# PARTIAL_LINK_TEXT: driver.find_element(By.PARTIAL_LINK_TEXT, "value")
+# CSS_SELECTOR: driver.find_element(By.CSS_SELECTOR, "value")
+
+# xpath locators:
+# //tagname[@attribute='value'] | //*[@attribute='value'] 
+# //tagname[@attribute='value' and @attribute='value']
+# //tagname[@attribute='value' or @attribute='value'] 
+# //tagname[contains(@attribute,'value')] 
+# //tagname[starts-with(@attribute,'value')] 
+# //tagname[text()='value']
+# //tagname[contains(text(),'value')] 
+# //tagname[starts-with(text(),'value')] 
+# //tagname[ends-with(text(),'value')]
+
+
+# following-sibling: //tagname[@attribute='value']/following-sibling::tagname
+# preceding-sibling: //tagname[@attribute='value']/preceding-sibling::tagname
+
 # ==============================================
 
 
@@ -10,10 +35,40 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://demo.automationtesting.in/Index.html")
 input_element = driver.find_element(By.ID, "email")
-wait = input_element.send_keys("manjunathc9942@gmail.com")
+wait = input_element.send_keys("manjunathc@gmail.com")
 driver.find_element(By.ID, "enterimg").click()
+
+first_name = driver.find_element(By.XPATH, "//input[@placeholder='First Name']")
+first_name.send_keys("Manjunath")
+last_name = driver.find_element(By.XPATH, "//input[@placeholder='Last Name']")
+last_name.send_keys("C")
+adress = driver.find_element(By.XPATH, "//textarea[@ng-model='Adress']")
+adress.send_keys("Bangalore")
+emailadress = driver.find_element(By.XPATH, "//input[@ng-model='EmailAdress']")
+emailadress.send_keys("manjunathc@gmail.com")
+phone = driver.find_element(By.XPATH, "//input[@ng-model='Phone']")
+phone.send_keys("1234567890")
+gender = driver.find_element(By.XPATH, "//input[@value='Male']")
+gender.click()
+Hobbies = driver.find_element(By.XPATH, "//input[@type='checkbox']")
+for hobby in Hobbies:
+    value = hobby.get_attribute("value")
+    if value == "Cricket":
+        hobby.click()
+        break           
+    if value == "Movies":
+        hobby.click()
+        break   
+    if value == "Hockey":
+        hobby.click()
+        break
+Languages = driver.find_element(By.ID, "Languagess")
+dropdown = Select(driver.find_element(By.ID, "Skills"))
+select = Select(dropdown_element)
+driver.find_element(By.XPATH, "//*[@value='Android']").click()
